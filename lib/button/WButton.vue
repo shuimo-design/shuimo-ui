643ca47b69553310baf498f916bc44e66b61a315
菩萨蛮 <higuaifan@higuaifan.com>
2021-02-24 15:14:43 +0800

    🚚 按钮组件的css单独提取出来

--- lib/button/WButton.vue
+++ lib/button/WButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="['w-button', {'w-button-disabled': disabled}, `w-button-${type}`]" :disabled="disabled">
+  <button :class="['w-button cursor-brush', {'w-button-disabled': disabled}, `w-button-${type}`]" :disabled="disabled">
     {{ text }}
   </button>
 </template>
@@ -20,30 +20,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.w-button {
-  cursor: pointer;
-  margin: 6px 0 0 10px;
-  padding: 0 15px;
-  height: 28px;
-  border: unset;
-  outline: unset;
-  line-height: 28px;
-  min-width: 70px;
-  border: 3px double transparent;
-  border-image: url("/lib/assets/button/button-border-blue.png") 10;
-  color: #192F6F;
-  text-align: center;
-}
-
-.w-button-disabled,
-.w-button-gray {
-  border-image: url("/lib/assets/button/button-border-gray.png") 10;
-  color: #333333;
-}
-
-.w-button:disabled {
-  cursor: not-allowed;
-}
-</style>
