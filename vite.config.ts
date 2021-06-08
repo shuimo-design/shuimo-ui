082fa2f6407e892be9a032b5783c28bca26a4c2a
菩萨蛮 <higuaifan@higuaifan.com>
2021-06-08 22:38:35 +0800

    👷 添加server部署命令及多环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  build: {
-    lib: {
-      name:'wash-painting-ui',
-      entry: 'lib/index.ts'
-    },
-    rollupOptions: {
-      external: ['vue']
+export default defineConfig((configEnv: ConfigEnv) => {
+  const { mode } = configEnv;
+  let build: any = {};
+  if (mode === 'npm') {
+    build = {
+      lib: {
+        name: 'wash-painting-ui',
+        entry: 'lib/index.ts'
+      },
+      rollupOptions: {
+        external: ['vue']
+      }
+    };
+  }
+  return {
+    build,
+    plugins: [vue()],
+    esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment'
     }
-  },
-  plugins: [vue()],
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment'
   }
 })
