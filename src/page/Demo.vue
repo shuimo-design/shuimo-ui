8dcab530c5bf6894e067b3ecba263cd13d24a606
菩萨蛮 <higuaifan@higuaifan.com>
2021-06-09 01:32:19 +0800

    🔥 删除不需要的import

--- src/page/Demo.vue
+++ src/page/Demo.vue
@@ -17,18 +17,6 @@
  * @author higuaifan
  * @date 2020/11/17 22:33
  **/
-import ButtonDemo from "./demos/ButtonDemo.vue";
-import InputDemo from "./demos/InputDemo.vue";
-import DialogDemo from "./demos/DialogDemo.vue";
-import TooltipDemo from "./demos/TooltipDemo.vue";
-import ScrollNumberDemo from "./demos/ScrollNumberDemo.vue";
-import SelectDemo from "./demos/SelectDemo.vue";
-import DatePickerDemo from "./demos/DatePickerDemo.vue";
-import PopoverDemo from "./demos/PopoverDemo.vue";
-import MessageDemo from "./demos/MessageDemo.vue";
-import FormDemo from "./demos/FormDemo.vue";
-import PaginationDemo from "./demos/PaginationDemo.vue";
-import UploadDemo from "./demos/UploadDemo.vue";
 
 export default {
   name: "Demo",
