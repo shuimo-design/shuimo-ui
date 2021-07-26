2b4f1c951d7e81a0a8e73b69026993ccbc19aeda
菩萨蛮 <higuaifan@higuaifan.com>
2021-07-26 11:52:01 +0800

    🎨 优化Mask模块代码结构

--- src/components/Mask.vue
+++ src/components/Mask.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="mask-div">
+    <div class="mask-main">
+      <slot></slot>
+    </div>
     <div class="mask-line mask-top-line"/>
     <div class="mask-line mask-left-line"/>
     <div class="mask-line mask-right-line"/>
     <div class="mask-line mask-bottom-line"/>
-    <div class="mask-main">
-      <slot></slot>
-    </div>
   </div>
 </template>
 
@@ -29,14 +29,22 @@ export default defineComponent({
 <style lang="scss" scoped>
 
 .mask-div {
+  display: grid;
+  grid-template-columns: 6px auto 6px;
+  grid-template-rows: 5px auto 5px;
+  grid-template-areas: "topleft top topright"
+                       "left main right"
+                       "bottomleft bottom bottomright";
   height: 100%;
   width: 100%;
 }
 
 .mask-line, .mask-main {
   display: inline-block;
+}
+
+.mask-line{
   position: relative;
-  z-index: 10;
 }
 
 .mask-top-line, .mask-bottom-line {
@@ -49,29 +57,38 @@ export default defineComponent({
   background: url(/src/assets/line/vertical.png) repeat-y;
   height: 100%;
   width: 6px;
-  top: -6px;
 }
 
-.mask-left-line {
-  left: -2px;
-}
-
-.mask-bottom-line {
-  bottom: 23px;
+.mask-top-line {
+  grid-area: topleft / topleft / topright / topright;
+  top: -2.5px;
 }
 
 .mask-right-line {
-  right: calc(-100% + 10px);
+  grid-area: topright / topright / bottomright / bottomright;
+  right: -3px;
 }
 
+.mask-bottom-line {
+  grid-area: bottomleft / bottomleft / bottomright / bottomright;
+  bottom: -2.5px;
+}
+
+.mask-left-line {
+  grid-area: topleft / topleft / bottomleft / bottomleft;
+  left: -3px;
+}
+
+
 .mask-main {
-  z-index: 1;
-  top: calc(-100% - 30px);
+  box-sizing: border-box;
+  grid-area: topleft / topleft / bottomright / bottomright;
   height: 100%;
   width: 100%;
+  padding: 20px;
+  overflow: auto;
   background: rgba(255, 255, 255, 0.4);
   backdrop-filter: blur(6px);
-  overflow: auto;
 }
 
 </style>
