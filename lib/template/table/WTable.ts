f9e4bb5a729a55fd6aa297c0c3a3d7b2a24a4cc4
菩萨蛮 <higuaifan@higuaifan.com>
2021-08-25 11:46:45 +0800

    🚀 table组件添加slot检查提升健壮性

--- lib/template/table/WTable.ts
+++ lib/template/table/WTable.ts
@@ -2,15 +2,17 @@
  * @Description: table组件
  * @Author: 菩萨蛮
  * @Date: 2021/8/23 11:30 上午
- * @Version v0.0.1-beta
+ * @Version v0.0.1-beta.1
  *
  * 一个极度简单的table组件
  * beta版 支持 slot width 设置
  *
  * 公司的业务千篇一律，复杂的代码好几百行。
+ * v0.0.1-beta.1 添加slot检查提升健壮性
  */
 import { defineComponent, h, VNode, VNodeNormalizedChildren } from 'vue'
 import { notEmpty } from "../../dependents/_utils/tools";
+import Printer from "../../other/printer/Printer";
 
 
 const img = h('div', { class: 'w-table-tbody-img' });
@@ -61,6 +63,12 @@ export default defineComponent({
   render(ctx: any) {
     const theadThList: VNode[] = [];
     const tbodyTrList: VNode[] = [];
+
+    if (!(ctx.$slots && ctx.$slots.default)) {
+      Printer('水墨UI表格组件').error('列表必须传入slot');
+      return null;
+    }
+
 
     // 如果表格slot不为空
     if (notEmpty(ctx.$slots.default())) {
