72f9cb8b8fe6abab2d7062956e937404f69b1a7c
菩萨蛮 <higuaifan@higuaifan.com>
2022-01-22 02:57:46 +0800

    🔧 修复width与预期不一致的问题，优化渲染逻辑

--- lib/other/progress/WProgress.tsx
+++ lib/other/progress/WProgress.tsx
@@ -2,33 +2,38 @@
  * @Description: 竹子进度条
  * @Author: 菩萨蛮
  * @Date: 2022/1/21 6:13 下午
- * @Version v1.0.0
+ * @Version v1.0.1
  *
  * 公司的业务千篇一律，复杂的代码好几百行。
+ * v1.0.1 修复width与预期不一致的问题，优化渲染逻辑
  */
 import { defineComponent } from "vue";
 import { isEmpty, notEmpty } from "../../dependents/_utils/tools";
 import leaf from '../../assets/progress/leaf.png';
 
-const BASE_WIDTH = 556;
-const BASE_HEIGHT = 37;
-const W2H = BASE_WIDTH / BASE_HEIGHT;
+const BASE_SIZE = {
+  BG: { W: 556, H: 37 },
+  LEAF: { W: 95, H: 109 },
+}
 
-const getSize = (w?: number, h?: number) => {
+const W2H = BASE_SIZE.BG.W / BASE_SIZE.BG.H;
+const LEAF_W2H = BASE_SIZE.LEAF.W / BASE_SIZE.LEAF.H;
+
+const getSize = (w?: number, h?: number, w2h = W2H) => {
   let size = {
-    width: w || BASE_WIDTH,
-    height: h || BASE_HEIGHT
+    width: w || BASE_SIZE.BG.W,
+    height: h || BASE_SIZE.BG.H
   }
   if (isEmpty(h) && notEmpty(w)) {
-    return { width: w!, height: w! / W2H }
+    return { width: w!, height: w! / w2h }
   }
   if (notEmpty(h) && isEmpty(w)) {
-    return { width: h! * W2H, height: h! }
+    return { width: h! * w2h, height: h! }
   }
   return size;
 };
-const getTextSize = (width: number, infoWidth: number, per: number) => {
-  return (width - infoWidth / 2) * per / 100 * 2;
+const getTextLeft = (width: number, perWidth: number, per: number) => {
+  return (width - perWidth / 2) * per / 100;
 }
 
 export default defineComponent({
@@ -40,32 +45,41 @@ export default defineComponent({
     max: { type: Number, default: 100 },
     showInfo: { type: Boolean, default: false },
     infoWidth: { type: Number, default: 44 },
-    style: { type: Object }
+    leafHeight: { type: Number, default: 28 }
   },
   render(ctx: any) {
+    const { showInfo, leafHeight } = ctx;
     const { width, height } = getSize(ctx.width, ctx.height);
-    const style = {
+    const style: Object = {
       '--w-progress-width': `${width}px`,
       '--w-progress-height': `${height}px`
     }
-    // @ts-ignore
-    const progress = (<progress className='w-progress' value={ctx.value} max={ctx.max} style={style}/>);
-    if (!ctx.showInfo) {
+    const progress = (<progress class='w-progress' value={ctx.value} max={ctx.max} style={style}/>);
+    if (!showInfo) {
       return progress;
     }
+    // 如果是有info的情况
+    const leafSize = getSize(undefined, leafHeight, LEAF_W2H);
+
+    const { infoWidth } = ctx;
 
     const per = Math.ceil(ctx.value / ctx.max * 100);
+    const perWidth = leafSize.width + infoWidth;
     const textStyle = {
-      '--w-progress-per-height': `${height}px`,
-      '--w-progress-per-widht': `${width}px`,
-      left: `${getTextSize(width, ctx.infoWidth, per)}px`
+      '--w-progress-per-height': `${leafSize.height}px`,
+      '--w-progress-per-width': `${perWidth}px`,
+      left: `${getTextLeft(width, perWidth, per)}px`
+    }
+
+    const baseStyle: Object = {
+      '--w-progress-leaf-height': `${leafHeight}px`
     }
 
     return (
-      <div class='w-progress-border' style={ctx.style}>
+      <div class={['w-progress-border', showInfo ? 'show-info' : '']} style={baseStyle}>
         <div class='w-progress-per' style={textStyle}>
-          <span>{per}%</span>
           <img class='leaf' src={leaf} alt=""/>
+          <span>{per}%</span>
         </div>
         {progress}
       </div>
