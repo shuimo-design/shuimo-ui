aad34fa5e5fb6e2504a5deec75ee6fe470264423
菩萨蛮 <higuaifan@higuaifan.com>
2022-02-13 01:18:20 +0800

    🚀 DialogDemo改为使用useDialog

--- src/page/demos/message/DialogDemo.vue
+++ src/page/demos/message/DialogDemo.vue
@@ -6,8 +6,8 @@
         <span>普通弹窗</span>
       </div>
     </w-dialog>
-    <w-button text="点击显示弹窗" @click="show"/>
-    <w-dialog :visible="dialogVisible" @close="close"
+    <w-button text="点击显示弹窗" @click="showDialog"/>
+    <w-dialog :visible="visible" @close="closeDialog"
               :mask="maskNoShowOption">
       <div class="height-600">
         <span>height:600</span>
@@ -28,24 +28,20 @@
  * @Description: dialog demo
  * @Author: 菩萨蛮
  * @Date: 2020/11/27 10:32 下午
- * @Version v1.0.1
+ * @Version v1.0.2
  *
  * 公司的业务千篇一律，复杂的代码好几百行。
  *  v1.0.1 改为setup script写法
+ *  v1.0.2 dialog参数管理使用useDialog
  */
-import { reactive, ref } from 'vue';
+import { reactive } from 'vue';
+import { useDialog } from '../../../../lib';
 
-const dialogVisible = ref(false);
-const dialogNormalVisible = ref(false);
-const dialogAdventureVisible = ref(false);
+const { visible, showDialog, closeDialog } = useDialog();
+const { visible: dialogNormalVisible, showDialog: showNormal, closeDialog: closeNormal } = useDialog();
+const { visible: dialogAdventureVisible, showDialog: showAdventure, closeDialog: closeAdventure } = useDialog();
+
 const maskNoShowOption = reactive({ show: false, clickClose: false });
-
-const show = () => {dialogVisible.value = true};
-const showNormal = () => {dialogNormalVisible.value = true};
-const close = () => {dialogVisible.value = false};
-const closeNormal = () => {dialogNormalVisible.value = false};
-const showAdventure = () => {dialogAdventureVisible.value = true};
-const closeAdventure = () => {dialogAdventureVisible.value = false};
 
 </script>
 
