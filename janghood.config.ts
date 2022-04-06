05c339cf8ae38338374ede524ab7162d6bc66adc
菩萨蛮 <higuaifan@higuaifan.com>
2022-04-06 23:06:04 +0800

    🧩 新增janghood/api-extractor组件

---
+++ janghood.config.ts
@@ -0,0 +1,32 @@
+/**
+ * @Description: 极客江湖插件配置文件
+ * @Author: 阿怪
+ * @Date: 2022/4/6 4:55 PM
+ * @Version v1.0.0
+ *
+ * 公司的业务千篇一律，复杂的代码好几百行。
+ */
+
+
+const firstUpperCase = (str: string) => {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+}
+
+const sourceSymbolTranslator = (dirList: string[]) => {
+  let lastDir = firstUpperCase(dirList[dirList.length - 1]);
+  return `W${lastDir}`;
+}
+
+// 后续可以扩展为defineConfig
+export default {
+  apiExtractor: {
+    include: ["lib"],
+    exclude: ['dependents'],
+    document: {
+      webTypesInfo: {
+        "framework": "vue",
+      },
+      sourceSymbolTranslator
+    }
+  }
+};
