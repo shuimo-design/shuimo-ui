2d9241992ff3a2589ba48339a7dbe73645169db5
菩萨蛮 <higuaifan@higuaifan.com>
2022-04-07 00:53:43 +0800

    👮 修改公共props类型

--- lib/dependents/_types/index.d.ts
+++ lib/dependents/_types/index.d.ts
@@ -6,12 +6,13 @@
  *
  * 公司的业务千篇一律，复杂的代码好几百行。
  */
-import { ComponentPropsOptions, ComponentPublicInstance } from "vue";
+import type { ComponentPublicInstance } from "vue";
+import type { ComponentObjectPropsOptions } from "@vue/runtime-core";
 
 /**
  * props和ctx类型接口
  */
 export declare interface IComponentOption<Props> {
-  props: ComponentPropsOptions<Props>;
+  props: ComponentObjectPropsOptions<Props>;
   ctx: ComponentPublicInstance<Props>;
 }
