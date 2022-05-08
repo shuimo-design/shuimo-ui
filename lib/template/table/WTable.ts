64c01e1fed9c7c50914a2578fbe80ce115caf093
higuaifan <higuaifan@higuaifan.com>
2022-05-08 20:33:41 +0800

    🚀 header样式根据height变换

--- lib/template/table/WTable.ts
+++ lib/template/table/WTable.ts
@@ -2,7 +2,7 @@
  * @description table组件
  * @author 阿怪
  * @date 2021/8/23 11:30 上午
- * @version v0.0.1-beta.2
+ * @version v0.0.2
  *
  * 一个极度简单的table组件
  * beta版 支持 slot width 设置
@@ -10,6 +10,7 @@
  * 公司的业务千篇一律，复杂的代码好几百行。
  * v0.0.1-beta.1 添加slot检查提升健壮性
  * v0.0.1-beta.2 新增header fixed功能
+ * v0.0.2 header样式根据height变换
  */
 import { defineComponent, h, VNode } from 'vue'
 import { notEmpty } from "../../dependents/_utils/tools";
@@ -60,44 +61,48 @@ type columnType = {
 export default defineComponent({
   name: 'WTable',
   props,
-  render(ctx: any) {
-    const theadThList: VNode[] = [];
-    const tbodyTrList: VNode[] = [];
+  setup(props, { slots }) {
+    return () => {
+      const theadThList: VNode[] = [];
+      const tbodyTrList: VNode[] = [];
 
-    if (!(ctx.$slots && ctx.$slots.default)) {
-      Printer('水墨UI表格组件').error('列表必须传入slot');
-      return null;
+      if (!(slots && slots.default)) {
+        Printer('水墨UI表格组件').error('列表必须传入slot');
+        return null;
+      }
+
+
+      // 如果表格slot不为空
+      if (notEmpty(slots.default())) {
+        const defaultSlot: any[] = slots.default();
+        const tableColumn: columnType[] = [];
+
+        // 遍历column
+        defaultSlot.forEach(s => {
+          const { param, label, width } = s.props;
+          tableColumn.push({
+            key: param,
+            children: s.children
+          });
+          // 构造thead
+          theadThList.push(h('th', { class: 'w-th', width }, label));
+        })
+        props.data.forEach((d: any) => {
+          tbodyTrList.push(dataTrRender(d, tableColumn));
+        })
+
+      }
+
+      const { theadBorderTop, theadBorderBottom, tbodyBorderBottom } = borderRender();
+
+      const thead = h('thead', {
+        class: ['w-thead', { 'w-thead-overflow': props.height }]
+      }, wrapWithTr(theadThList));
+      const tbody = h('tbody', { class: 'w-tbody' }, tbodyTrList);
+      const table = h('table', { class: 'w-table-inner' }, [thead, tbody]);
+      const tableWrap = h('div', { class: 'w-table-wrap', style: { height: props.height } }, table);
+
+      return h('div', { class: 'w-table' }, [theadBorderTop, theadBorderBottom, tableWrap, tbodyBorderBottom]);
     }
-
-
-    // 如果表格slot不为空
-    if (notEmpty(ctx.$slots.default())) {
-      const defaultSlot: any[] = ctx.$slots.default();
-      const tableColumn: columnType[] = [];
-
-      // 遍历column
-      defaultSlot.forEach(s => {
-        const { param, label, width } = s.props;
-        tableColumn.push({
-          key: param,
-          children: s.children
-        });
-        // 构造thead
-        theadThList.push(h('th', { class: 'w-th', width }, label));
-      })
-      ctx.data.forEach((d: any) => {
-        tbodyTrList.push(dataTrRender(d, tableColumn));
-      })
-
-    }
-
-    const { theadBorderTop, theadBorderBottom, tbodyBorderBottom } = borderRender();
-
-    const thead = h('thead', { class: 'w-thead' }, wrapWithTr(theadThList));
-    const tbody = h('tbody', { class: 'w-tbody' }, tbodyTrList);
-    const table = h('table', { class: 'w-table-inner' }, [thead, tbody]);
-    const tableWrap = h('div', { class: 'w-table-wrap', style: { height: ctx.height } }, table);
-
-    return h('div', { class: 'w-table' }, [theadBorderTop, theadBorderBottom, tableWrap, tbodyBorderBottom]);
-  }
+  },
 })
