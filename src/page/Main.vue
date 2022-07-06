53e2256e7f22ab593db7b9e5779d52d325fc7bfd
higuaifan <higuaifan@higuaifan.com>
2022-07-07 01:07:15 +0800

    🔥 移除demo，src模块改为playground

--- src/page/Main.vue
+++ src/page/Main.vue
@@ -1,53 +1,51 @@
 <template>
   <div class="base">
-    <img src="/src/assets/logo.png" alt="">
-    <h1>一款粗暴的水墨UI</h1>
-    <span>基于Vite2.0 + Vue3</span>
-    <br/>
-    <span>运用于 <m-button text="极客江湖" @click="toGeekjx"/></span>
-    <m-divider/>
-    <m-form>
-      <m-form-item label="跳转到：">
-        <m-button text="github" @click="toGithub"/>
-      </m-form-item>
-      <m-form-item label="跳转到：">
-        <m-button text="npm" @click="toNpm"/>
-      </m-form-item>
-    </m-form>
+    <h1>这里是playground</h1>
+
+
+    <m-border>
+      <div class="test m-scroll">
+        <div class="inside"/>
+      </div>
+    </m-border>
+
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 /**
- * @description 主页
+ * @description playground
  * @author 阿怪
  * @date 2021/6/8 11:42 下午
  * @version v1.0.0
  *
  * 公司的业务千篇一律，复杂的代码好几百行。
  */
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'Main',
-  methods: {
-    toGeekjx() {
-      window.open('https://geekjx.com');
-    },
-    toGithub() {
-      window.open('https://github.com/higuaifan/shuimo-ui');
-    },
-    toNpm() {
-      window.open('https://www.npmjs.com/package/@janghood/shuimo-ui');
-    },
-  }
-});
 </script>
 
 <style lang="scss" scoped>
-
 .base {
-  margin: 10px 20px;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
 }
 
+.test {
+  height: 400px;
+  width: 400px;
+  overflow: auto;
+
+}
+
+.inside {
+  height: 5000px;
+  width: 1200px;
+  background: linear-gradient(0deg,  #ffc700 0%, #e91e1e 100%);
+
+}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,38 +7,13 @@
  * 公司的业务千篇一律，复杂的代码好几百行。
  */
 import { createRouter, createWebHistory } from 'vue-router';
-import { base } from "./component/base";
-import { template } from "./component/template";
-import { message } from "./component/message";
-import { other } from "./component/other";
 
 const routes = [
   {
     path: '/',
-    name: 'Entry',
-    component: () => import('../page/Entry.vue'),
+    name: 'Main',
+    component: () => import('../page/Main.vue'),
   },
-  {
-    path: '/demo',
-    name: 'Demo',
-    component: () => import('../page/Demo.vue'),
-    children: [
-      {
-        path: '',
-        name: 'base',
-        component: () => import('../page/Main.vue')
-      },
-      {
-        path: '/color',
-        name: 'color',
-        component: () => import('../page/demos/ColorDemo.vue')
-      },
-      ...base,
-      ...template,
-      ...message,
-      ...other,
-    ]
-  }
 ];
 
 const router = createRouter({
