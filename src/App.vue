4813cbcb94e82b82349df1e95dc140abd33e0637
higuaifan <higuaifan@higuaifan.com>
2022-07-15 00:28:47 +0800

    🚀 playground添加宣纸背景

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <router-view/>
+  <m-rice-paper class="main-paper">
+    <router-view/>
+  </m-rice-paper>
 </template>
+
+
+<style lang="scss" scoped>
+.main-paper {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
