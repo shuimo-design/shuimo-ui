1936495260490599ce72ec39b98b2caddfd1f51e
higuaifan <higuaifan@higuaifan.com>
2022-08-04 10:24:16 +0800

    ⚡️ 使用border方式实现边框模块

--- lib/base/button/MButton.tsx
+++ lib/base/button/MButton.tsx
@@ -2,13 +2,14 @@
  * @description 按钮组件
  * @author 阿怪
  * @date 2021/8/10 4:59 下午
- * @version v2.0.0
+ * @version v2.1.0
  *
  * 公司的业务千篇一律，复杂的代码好几百行。
  * v1.0.1 新增slot文本形式
  * v1.0.2 优化结构
  * v1.0.3 添加link属性
  * v2.0.0 升级交互
+ * v2.1.0 使用border方式实现边框模块
  */
 import { h, defineComponent } from 'vue';
 import { props } from "./api";
@@ -22,14 +23,10 @@ export default defineComponent({
     return () => {
       let buttonText = slots.default && slots.default() || text;
       const domType = props.link ? 'a' : 'button';
-      const button = h(domType, {
-        class: ['m-button-base'],
+      return h(domType, {
+        class: ['m-button', { 'm-button-disabled': disabled }, `m-button-${type}`],
         disabled: disabled
-      }, buttonText)
-      return h(MButtonBorder, {
-          class: ['m-button', `m-button-${type}`, { 'm-button-disabled': disabled }]
-        },
-        () => button);
+      }, buttonText);
     }
   }
 })
