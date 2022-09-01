250246da842d5967b7e684857a636e9102dbec15
higuaifan <higuaifan@higuaifan.com>
2022-09-01 11:27:28 +0800

    🚀 【table】添加无数据提示和empty插槽

--- lib/template/table/MTable.ts
+++ lib/template/table/MTable.ts
@@ -2,7 +2,7 @@
  * @description table组件
  * @author 阿怪
  * @date 2021/8/23 11:30 上午
- * @version v0.0.2
+ * @version v0.0.3
  *
  * 一个极度简单的table组件
  * beta版 支持 slot width 设置
@@ -11,6 +11,7 @@
  * v0.0.1-beta.1 添加slot检查提升健壮性
  * v0.0.1-beta.2 新增header fixed功能
  * v0.0.2 header样式根据height变换
+ * v0.0.3 添加无数据提示和empty插槽
  */
 import { defineComponent, h, VNode } from 'vue'
 import { notEmpty } from "../../dependents/_utils/tools";
@@ -67,9 +68,12 @@ export default defineComponent({
       const tbodyTrList: VNode[] = [];
 
       if (!(slots && slots.default)) {
-        Printer('水墨UI表格组件').error('列表必须传入slot');
+        Printer('水墨UI表格组件').error('列表必须传入一个默认的m-table-column');
         return null;
       }
+
+      const emptySlot = slots && slots.empty ? slots.empty() :
+        h('div', { class: 'm-table-empty' }, '暂无数据');
 
 
       // 如果表格slot不为空
@@ -100,7 +104,9 @@ export default defineComponent({
       }, wrapWithTr(theadThList));
       const tbody = h('tbody', { class: 'm-tbody' }, tbodyTrList);
       const table = h('table', { class: 'm-table-inner' }, [thead, tbody]);
-      const tableWrap = h('div', { class: 'm-table-wrap', style: { height: props.height } }, table);
+      const tableWrap = h('div',
+        { class: 'm-table-wrap', style: { height: props.height } },
+        [table, notEmpty(props.data) ? undefined : emptySlot]);
 
       return h('div', { class: 'm-table' }, [theadBorderTop, theadBorderBottom, tableWrap, tbodyBorderBottom]);
     }
