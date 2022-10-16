41659b3f8696a2af5b9da0cd27bec0af1a54439a
youus <alecshen0926@foxmail.com>
2022-10-16 17:12:26 +0800

    🔧 覆盖率配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     coverage: {
       all: true,
       include: ['lib/**'],
-      exclude: ['lib/index.ts', 'lib/**/*.d.ts'],
+      exclude: ['lib/index.ts', 'lib/**/*.d.ts', 'lib/**/*.d.tsx'],
       reporter: ['json', 'html']
     }
   }
