10665527b0febd7c6659c09ba086aa8ae832aae7
你好怪凡 <higuaifan@higuaifan.com>
2023-01-10 15:09:22 +0800

    🔧 [checkbox] fix import relative path error

--- packages/web-component/lib/base/checkbox/index.ts
+++ packages/web-component/lib/base/checkbox/index.ts
@@ -7,7 +7,7 @@
  * 江湖的业务千篇一律，复杂的代码好几百行。
  */
 
-import { createMElement, MElement } from '../../../../../tools/m-component/index';
+import { createMElement, MElement } from 'moelement';
 import { useCheckbox } from '@shuimo-design/core';
 import { CheckboxProps } from '@shuimo-design/core/lib/base/checkbox';
 
