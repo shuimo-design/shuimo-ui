c133b59b79167ee781d1051ad971b212862eb16f
你好怪凡 <higuaifan@higuaifan.com>
2023-01-11 17:34:49 +0800

    🔧 [playground] modify web-component import

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import '@shuimo-design/web-component';
+import '@shuimo-design/web-component/index.ts';
 import './assets/style.css';
 
 
