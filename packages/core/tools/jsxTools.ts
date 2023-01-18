9952e912b07eef16e051570bb8edd01bebda36ed
你好怪凡 <higuaifan@higuaifan.com>
2023-01-18 18:05:00 +0800

    🔧 [core-jsxTools] fix web-component render error

--- packages/core/tools/jsxTools.ts
+++ packages/core/tools/jsxTools.ts
@@ -8,8 +8,8 @@
  */
 import { MNodeTemplate } from '../types';
 
-
-export const mWC = (type: string, props?: Record<string, any> | null, ...childList: MNodeTemplate[]) => {
+export const m = (type: string, props?: Record<string, any> | null, ...childList: MNodeTemplate[]) => {
+  // for vue
   const children: MNodeTemplate['children'] = {};
   const slots: MNodeTemplate['slots'] = new Map();
   for (let c of childList) {
@@ -27,20 +27,48 @@ export const mWC = (type: string, props?: Record<string, any> | null, ...childLi
       children[name] = c;
     }
   }
-  return { type, props, children, slots, if: true, show: true };
+  const propsIf = props && props['m-if'];
+  const propsShow = props && props['m-show'];
+  return { type, props, children, slots, if: mBoolean(propsIf), show: mBoolean(propsShow) };
 };
 
-export const m = (type: string, propsRecord?: Record<string, any> | null, ...slots: any) => {
+export const mWC = (type: string, propsRecord?: Record<string, any> | null, ...childList: MNodeTemplate[]) => {
+  // for web-component, web-component template depth must less than 2
   let props = propsRecord || {};
   if (type === 'slot') {
-    if (!props || !props.name) {
-      props.name = 'default';
+    props.name = !props || !props['m-name'] ? 'default' : props['m-name'];
+  }
+  const propsIf = props && props['m-if'];
+  const propsShow = props && props['m-show'];
+
+
+  const slots = new Map();
+  const children: MNodeTemplate['children'] = {};
+  if (childList) {
+    for (let c of childList) {
+      if (c.type === 'slot') {
+        slots.set(c.props!.name, c);
+      } else {
+        const name = c.props!['m-name'] as string;
+        if (!name) {
+          console.warn('m-name is required for child of web-component');
+        }
+        children[name] = c;
+      }
     }
   }
-  return { type, props, slots };
+  return { type, props, children, slots, if: mBoolean(propsIf), show: mBoolean(propsShow) };
 };
 
 
 export const shuimoJsxFragment = (...arg: any) => {
   console.log(...arg);
+};
+
+const mBoolean = (v: string | boolean | undefined, defaultValue = true) => {
+  if (v === 'false') {return false;}
+  if (v === 'true') {return true;}
+  if (v === undefined || v === null) {return defaultValue;}
+  console.warn('m-if or m-show only support string "true" or "false"');
+  return false;
 };
