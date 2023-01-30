60de7f53d72a9d13b0c2a199a2ebb3ebef4957be
你好怪凡 <higuaifan@higuaifan.com>
2023-01-30 22:55:19 +0800

    👮 [vue] fix MNodeTemplate import error

--- packages/vue/tools/coreRender.ts
+++ packages/vue/tools/coreRender.ts
@@ -7,8 +7,7 @@
  * 江湖的业务千篇一律，复杂的代码好几百行。
  */
 import { h, Slots, VNode } from 'vue';
-import { MNodeTemplate } from '@shuimo-design/core';
-import { MCOPO } from '@shuimo-design/types';
+import { MCOPO, MNodeTemplate } from '@shuimo-design/types';
 
 
 export const cr = <T>(
