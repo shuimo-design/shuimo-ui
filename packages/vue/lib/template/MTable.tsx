eb5ebad373392036ea2a0c3f275f8128c9c16326
你好怪凡 <higuaifan@higuaifan.com>
2023-07-14 14:33:25 +0800

    🐛 [vue/table] fix props

--- packages/vue/lib/template/MTable.tsx
+++ packages/vue/lib/template/MTable.tsx
@@ -5,6 +5,8 @@
  * @version v1.0.0
  *
  * 江湖的业务千篇一律，复杂的代码好几百行。
+ *
+ * todo fix when header codes more then one line
  */
 import { defineComponent, Fragment, Comment } from 'vue';
 import { props } from '@shuimo-design/core/lib/template/table/api';
