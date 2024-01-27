580ed5203231557d832f5c629bd92b51cf759e0b
youus <alecshen0926@foxmail.com>
2024-01-27 23:12:59 +0800

    🚀 [breadcrumb] Split to breadcrumbItem types

---
+++ lib/components/template/breadcrumb/breadcrumbItem.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @description breadcrumb item api type
+ * @author youus
+ * @date 2023/12/19 23:05
+ * @version v1.0.0
+ *
+ *
+ * @name m-breadcrumb-item
+ * @docDescription Breadcrumb component with shuimo-ui style.
+ *                  水墨组件的面包屑组件。
+ * @docUrl https://shuimo.design/breadcrumb
+ *
+ * Hello, humor
+ */
+
+export declare type BreadcrumbItemProps = {
+  /**
+   * @description 面包屑项内容
+   * @type string
+   * @default ''
+   */
+  content?:string,
+}
